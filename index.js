bf9d6fd6bde0721b400aaf1de46b953fe9f10973
Bot5te <141955189+Bot5te@users.noreply.github.com>
2025-12-07 02:37:07 +0200

    Update index.js

--- index.js
+++ index.js
@@ -145,7 +145,7 @@ async function startScheduler(sock) {
             const todayStr = format(nowEgypt, "yyyy-MM-dd");
 
             // من 8:00 إلى 8:44 صباحًا (يمكنك تغييرها إلى 10:00 زي ما تحب)
-            if (hour === 1 && minute < 55 && lastSentDate !== todayStr) {
+            if (hour === 3 && minute < 55 && lastSentDate !== todayStr) {
 
                 console.log(`\n[${format(nowEgypt, "HH:mm:ss")}] جاري البحث عن ورديات الغد في الـ Gist...`);
 
